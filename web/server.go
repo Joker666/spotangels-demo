577ea87848c8789c8f0d7f3648a46a0e978095c4
Hasan Rafi <mr.k779@outlook.com>
2021-08-16 05:57:38 +0600

    Added status route

--- web/server.go
+++ web/server.go
@@ -10,4 +10,7 @@ import (
 // NewServer adds routes to mux
 func NewServer(router *mux.Router, regulationController *controller.RegulationController) {
 	router.HandleFunc("/regulation/{segment_id}", regulationController.GetActiveRegulation).Methods(http.MethodGet)
+	router.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Ok"))
+	})
 }
