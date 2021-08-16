c4be40f2217a04d18c74faf72bb569f70acea82b
Hasan Rafi <mr.k779@outlook.com>
2021-08-16 06:17:58 +0600

    Added docker compose init script

--- web/server.go
+++ web/server.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"github.com/Joker666/spotangels-demo/web/controller"
+	"github.com/Joker666/spotangels-demo/web/resp"
 	"github.com/gorilla/mux"
 )
 
 // NewServer adds routes to mux
 func NewServer(router *mux.Router, regulationController *controller.RegulationController) {
-	router.HandleFunc("/regulation/{segment_id}", regulationController.GetActiveRegulation).Methods(http.MethodGet)
 	router.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Ok"))
+		resp.ServeData(w, r, http.StatusOK, "Ok")
 	})
+	router.HandleFunc("/regulation/{segment_id}", regulationController.GetActiveRegulation).Methods(http.MethodGet)
 }
